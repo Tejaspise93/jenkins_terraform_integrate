907740f5dfd5af42bd11ab1246a0028a8d1130c4
spongebob <tejaspise931259@gmail.com>
2026-02-19 15:45:10 +0530

    subnet2 added

--- main.tf
+++ main.tf
@@ -26,3 +26,12 @@ resource "aws_subnet" "my_subnet_1" {
     Name = "my_subnet_1"
   }
 }
+
+resource "aws_subnet" "my_subnet_2" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "my_subnet_2"
+  }
+}
